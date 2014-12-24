2db875a8c60cc27054708e914a6980be0bb9f623
Lavmira <marina.lavrova2012@yandex.ru>
2014-12-24 16:54:41 +0300

    По ДЗ 5

---
+++ view/all.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Новости</title>
+</head>
+<body>
+/**
+* @var array $articles Список всех новостей
+*/
+?>
+<h1>TEST</h1>
+
+<?php foreach ($articles as $art): ?>
+    <h2><?= $art['title']; ?></h2>
+
+    <article>
+        <h1><?=$art['title'];?></h1>
+        <div><?=$art['text'];?></div>
+    </article>
+    <?php endforeach; ?>
+
+</body>
+</html>
+
