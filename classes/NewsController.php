50ddfe3c6ab1e8463b8893aed83c374f9f85bc9e
Lavmira <marina.lavrova2012@yandex.ru>
2014-12-24 16:53:52 +0300

    По ДЗ 5

--- classes/NewsController.php
+++ classes/NewsController.php
@@ -4,32 +4,67 @@
 class NewsController
 extends AController
 {
+    private $view;
+    private $model;
+    public function __construct()
+    {
+        $this->view = new View();
+        $this->model = new ModelNews();
+    }
+    // главная страница
+    protected function actionIndex()
+    {
+        $this->view->news = $this->model->findAll();
+        $this->view->display('view/index.php');
+    }
+
+    // просмотр всех новостей
     protected function actionAll()
     {
-        $view = new View();
-        $view->news = News_getAll();
-
-        $view->display('view/index.php');
+        $this->view->news = $this->model->findAll();
+        $this->view->display('view/all.php');
     }
+    //просмотр 1-ной новости
     protected function actionOne()
     {
-        $view = new View();
-        $view->article = News_getOne($_GET['id']);
-
-        $view->display('view/one.php');
+        $this->view->one_article = $this->model->findByPk($_GET['id']);
+        $this->view->display('view/one.php');
     }
+//форма добавления новости
+    protected function actionFormAdd()
+    {
+        $this->view->display('view/save.php');
+    }
+    //добавление новости
+    protected function actionAdd()
+    {
+        if (isset($_POST['title']) && isset($_POST['text']))
+        {
+            $this->model->insertArticle($_POST['title'], $_POST['text']);
+            header("Location: all.php");
+        }
+    }
+    //форма редактирования новости
+    protected function actionFormEdit()
+    {
+        $this->view->edit_article = $this->model->findByPk($_GET['id']);
+        $this->view->display('view/edit.php');
+    }
+    // update новости
     protected function actionEdit()
     {
-        $view = new View();
-        $view->article = News_getEdit($_GET['id']);
-
-        $view->display('view/edit.php');
+        if (isset($_POST['title']) && isset($_POST['text']))
+        {
+            $this->model->updateArticle($_POST['id'],$_POST['title'],$_POST['text']);
+        }
+        header("Location: all.php");
     }
-    protected function actionSave()
+    //удаление новости
+    protected function actionDelete()
     {
-        $view = new View();
-        $view->article = News_getSave($_GET['id']);
-
-        $view->display('view/save.php');
+        $this->model->deleteArticle($_GET['id']);
+        header("Location: all.php");
     }
+}
+
 } 
