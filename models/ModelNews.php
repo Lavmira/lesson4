51be975bfdf3142df0198f7629ec1b500f89f44e
Lavmira <marina.lavrova2012@yandex.ru>
2014-12-24 16:43:05 +0300

    По ДЗ 5

---
+++ models/ModelNews.php
@@ -0,0 +1,39 @@
+<?php
+
+class ModelNews
+    extends AbstractModel
+{
+    static protected $table = 'news';
+    static function updateArticle($id, $new_title, $new_text)
+    {
+        $sql = "UPDATE " . static:: . $table" SET title=:new_title, text=:new_text  WHERE id=:id";
+        $db = new DBConnection();
+        return $db->toExecute($sql,[':id'=>$id,':new_title'=>$new_title,':new_text'=>$new_text]);
+    }
+    static function insertArticle($title,$text)
+    {
+        $sql = "INSERT INTO " . static::$table . " (title,text) VALUES (:title, :text)";
+        $db = new DBConnection();
+        return $db->toExecute($sql,[':title'=>$title,':text'=>$text]);
+    }
+    static function deleteArticle($id)
+    {
+        $sql = "DELETE FROM " . static::$table . " WHERE id=:id";
+        $db = new DBConnection();
+        return $db->toExecute($sql,[':id'=>$id]);
+    }
+}
+
+
+/*
+require_once __DIR__ . '/functions/DBConnect.php';
+require_once __DIR__ . '/classes/AModel.php';
+
+function News_getAll()
+{
+    return DBQuery("
+    SELECT * FROM news
+    ");
+}
+
+
