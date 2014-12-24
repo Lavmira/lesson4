6922f5e4a1dcf7ede6e20352bca907ffe9836bd3
Lavmira <marina.lavrova2012@yandex.ru>
2014-12-24 16:56:36 +0300

    По ДЗ 5

--- boot.php
+++ boot.php
@@ -1,19 +1,11 @@
 <?php
-function __autoload($class)
-{
-    require __DIR__ . '/classes/' . $class . '.php';
-}
-
-require_once __DIR__ . '/models/news.php';
-require_once __DIR__ . '/function/db.php';
-
-/*
 
 function __autoload($class)
 {
+
     $artClassDir = ["classes", "controller", "models"];
     {
-        $classPath = __DIR__ . "/../" . $folder . "/" . $class . ".php";
+        $classPath = __DIR__ . "/../" . $artClassDir . "/" . $class . ".php";
 
         if(is_readable($classPath))
         {
@@ -22,4 +14,29 @@ function __autoload($class)
         }
     }
 }
-*/
+
+
+
+/*
+
+
+
+function __autoload($class)
+{
+    require __DIR__ . '/classes/' . $class . '.php';
+}
+
+require_once __DIR__ . '/models/ModelNews.php';
+;
+
+spl_autoload_register('autoloadClasses');
+
+function autoloadModels($class)
+{
+    $filename = __DIR__ .'/models/' . $class . '.php';
+    if (file_exists($filename))
+    {
+        require_once $filename;
+    }
+}
+spl_autoload_register('autoloadModels');*/
