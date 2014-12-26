5d10710f873e619927245073a42473f5135a196a
Lavmira <marina.lavrova2012@yandex.ru>
2014-12-26 19:41:00 +0300

    к ДЗ 6

--- index.php
+++ index.php
@@ -1,14 +1,39 @@
 <?php
+//require_once __DIR__. '/model/ModelNews.php';
+//require_once __DIR__. 'view/index.php';
 
-require __DIR__ . '/boot.php';
-require __DIR__ . '/classes/AController.php';
-// r=news/all
+//единая точка входа
 
+// подключаем автозагрузку классов
+
+require_once __DIR__. '/boot.php';
+
+//задаем стартовую страницу
+
+$controllerClassName = 'NewsController';
+$actionPart = 'index';
+
+//роутер
+//?r=news/one&id=1
+//здесь используем контроллер newsController, метод actionOne, id - номер новости
 $route = $_GET['r'];
-$routeParts = explode('/', $route);
-$controllerClassName = ucfirst($routeParts[0] . 'Controller');
-$actionName = $routeParts[1];
-$controller = new $controllerClassName();
-$controller->action('$routeParts[1]');
+if (null !== $route)
+{
+    $routeParts = explode('/',$route);
+    $controllerClassName = ucfirst($routeParts[0]).'Controller';
+    $actionPart = $routeParts[1];
+}
+
+//фронт-контроллер
+$controller = new $controllerClassName;
+$controller->action($actionPart);
+
+
+
+
+
+
+
+
 
 
